c387c1876bdaf6f37367d68ce8a4f69b50cf465d
cskhw <gusdn0828@gmail.com>
2020-05-29 15:08:23 +0900

    Revert "Merge branch 'master' of C:\Users\gusdn\Desktop\project\InstaClone with conflicts."
    This reverts commit c2a3315f125e391e3bca230f9955697532654969.

--- app/src/main/java/com/connple/weat/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/connple/weat/navigation/DetailViewFragment.kt
@@ -1,20 +1,133 @@
 package com.connple.weat.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.connple.weat.R
+import com.connple.weat.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.item_detail.view.*
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_detail.view.*
 
 
-class DetailViewFragment : Fragment(){
+class DetailViewFragment : Fragment() {
+    var firestore: FirebaseFirestore? = null
+    var uid: String? = null
+    private var mainView: View? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)
-        return super.onCreateView(inflater, container, savedInstanceState)
+        firestore = FirebaseFirestore.getInstance()
+        uid = FirebaseAuth.getInstance().currentUser?.uid
+
+        mainView?.detailviewfragment_recyclerview?.layoutManager = LinearLayoutManager(activity)
+        mainView?.detailviewfragment_recyclerview?.adapter = DetailRecyclerViewAdapter()
+        mainView = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
+        return mainView
+    }
+
+    inner class DetailRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList: ArrayList<String> = arrayListOf()
+        init {
+            firestore?.collection("image")?.orderBy("timestamp")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                contentDTOs.clear()
+                contentUidList.clear()
+                for (snapshot in querySnapshot!!.documents) {
+                    var item = snapshot.toObject(ContentDTO::class.java)
+                    contentDTOs.add(item!!)
+                    contentUidList.add(snapshot.id)
+                }
+                notifyDataSetChanged()
+            }
+        }
+
+
+
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(p0.context).inflate(R.layout.item_detail, p0, false)
+            return CustomViewHolder(view)
+        }
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+        }
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+            var viewholder = (p0 as CustomViewHolder).itemView
+
+            // UserId
+            viewholder.detailviewitem_profile_textview.text = contentDTOs!![p1].userId
+
+            // Image
+            Glide.with(p0.itemView.context).load(contentDTOs!![p1].imageUrl)
+                .into(viewholder.detailviewitem_imageview_content)
+
+            // Explain
+            viewholder.detailviewitem_explain_textview.text = contentDTOs!![p1].explain
+
+            // Likes
+            viewholder.detailviewitem_favoritecounter_textview.text =
+                "Likes " + contentDTOs!![p1].favoriteCount
+
+            //This code is when the page is loaded
+            viewholder.detailviewitem_favorite_imageview.setOnClickListener {
+                favoriteEvent(p1)
+            }
+            if (contentDTOs!![p1].favorites.containsKey(uid)) {
+                //this is like status
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite)
+            } else {
+                // this is unlike status
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite_border)
+            }
+
+            //This code is when the profile image is clicked
+            viewholder.detailviewitem_profile_image.setOnClickListener {
+                var userFragment = UserViewFragment()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                bundle.putString("destinationUid",contentDTOs[p1].uid)
+                userFragment.arguments = bundle
+                activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.main_content,userFragment)?.commit()
+
+            }
+            viewholder.detailviewitem_comment_imageview.setOnClickListener { v ->
+                var intent = Intent(v.context, CommentActivity::class.java)
+                intent.putExtra("contentUid", contentUidList[p1])
+                startActivity(intent)
+            }
+        }
+
+        fun favoriteEvent(position: Int) {
+            var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction { transaction ->
+
+                var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
+
+                if (contentDTO!!.favorites.containsKey(uid)) {
+                    //When the button is clicked
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount - 1
+                    contentDTO?.favorites.remove(uid)
+                } else {
+                    //When the button is not clicked
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount + 1
+                    contentDTO?.favorites[uid!!] = true
+                }
+                transaction.set(tsDoc, contentDTO)
+            }
+        }
     }
 }
